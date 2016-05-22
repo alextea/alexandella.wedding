7228c713613f65f01dd627e002368066b6fae691
Alex Torrance <hello@alextorrance.co.uk>
2016-05-22 22:02:26 +0100

    Home page updates

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
-<?php 
+<?php
   $page_title = "Ella &amp; Alex's Wedding";
   include "includes/head.php";
 ?>
 
   <div id="wrapper">
     <main id="content">
-      <?php 
-        $active = "intro";
+      <?php
+        $active = "home";
         include "includes/header.php";
       ?>
 
-      <section id="intro">
+      <section id="home">
         <h2>When</h2>
 
         <h3>Ceremony</h3>
-        
+
         <p>The wedding ceremony begins at 12 noon. Please arrive at 11.30am.</p>
 
         <h3>Evening reception</h3>
-        
-        <p>Evening reception from 6pm.</p>
+
+        <p>Evening reception from 6pm until late</p>
 
         <p>We’re hoping for an amazing sunset, and to go with it we’ll be having cocktails and finger foods and some amazing DJs to dance the night away.</p>
-        
+
         <h3>What to wear</h3>
 
         <p>Summer chic, smart and bright. Hats and sunglasses a must.</p>
