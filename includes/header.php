2fba8822d1f21cf5df6a32be021ec01b39c8b776
Alex Torrance <hello@alextorrance.co.uk>
2016-05-22 22:02:14 +0100

    Header layout update

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,14 @@
 <header id="header">
-  <h1>Alex &amp; Ella's Wedding</h1>
-  <h2>6 August 2016</h2>
-  <p>Southern Pavilion, Worthing Pier</p>
+  <div>
+    <h1>Ella &amp; Alex's Wedding</h1>
+    <h2>6 August 2016</h2>
+    <p>Southern Pavilion, Worthing Pier</p>
+  </div>
 </header>
 
 <nav id="navigation">
   <ul>
-    <li<?php if ($active == "when") { ?> class="active"<?php } ?>><a href="/">When</a></li>
+    <li<?php if ($active == "home") { ?> class="active"<?php } ?>><a href="/">When</a></li>
     <li<?php if ($active == "where") { ?> class="active"<?php } ?>><a href="/where">Where</a></li>
     <li<?php if ($active == "rsvp") { ?> class="active"<?php } ?>><a href="/rsvp">RSVP</a></li>
     <li<?php if ($active == "gifts") { ?> class="active"<?php } ?>><a href="/gifts">Gifts</a></li>
