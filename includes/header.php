cfc4e35c904212c9cf3af29cb602c7e30ac74155
Alex Torrance <hello@alextorrance.co.uk>
2016-05-24 22:44:18 +0100

    Remove unused menu items

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,5 @@
     <li<?php if ($active == "home") { ?> class="active"<?php } ?>><a href="/">When</a></li>
     <li<?php if ($active == "where") { ?> class="active"<?php } ?>><a href="/where">Where</a></li>
     <li<?php if ($active == "rsvp") { ?> class="active"<?php } ?>><a href="/rsvp">RSVP</a></li>
-    <li<?php if ($active == "gifts") { ?> class="active"<?php } ?>><a href="/gifts">Gifts</a></li>
-    <li<?php if ($active == "photos") { ?> class="active"<?php } ?>><a href="/photos">Photos</a></li>
   </ul>
 </nav>
